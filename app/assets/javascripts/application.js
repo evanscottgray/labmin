10e1b95ec38bf6c83a8988105afc8d90444d2004
Evan Gray <evan.gray@rackspace.com>
2014-09-14 10:51:21 -0500

    add alerts css and canon js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,35 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function ready () {
+    // Attach all Canon tooltips.
+    $('.tooltip-toggle').each(function (i, element) {
+        var tooltip;
+        tooltip = new canon.Tooltip();
+        tooltip.attach(element);
+    });
+
+    // Attach all Canon dropdowns.
+    $('.rs-dropdown').each(function (i, element) {
+        var menu = new canon.Menu();
+        menu.attach(element);
+    });
+
+    // Animate in alerts if there are any.
+    $('.alert').each(function(i, element) {
+        $(element).delay(300).animate({ opacity: 1 }, 700)
+    });
+    // Collapse ALL the sections!! _o/
+    $(".rs-collapsible-section .rs-detail-section-header").click(function(){
+        $(this).parent().toggleClass("collapsed expanded");
+    });
+};
+
+$('document').ready(function (){
+    ready();
+});
