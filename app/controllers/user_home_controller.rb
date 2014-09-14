cba8677aad32157cc309f819ee1102ff78410276
Evan Gray <evan.gray@rackspace.com>
2014-09-14 12:59:02 -0500

    added user controller stuff

--- app/controllers/user_home_controller.rb
+++ app/controllers/user_home_controller.rb
@@ -1,3 +1,7 @@
 class UserHomeController < ApplicationController
   before_filter :authenticate_user!
+  def index
+    user = User.find_by_id(current_user.id)
+    @page = {:user => user}
+  end
 end
