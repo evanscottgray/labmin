eaa1381a653100177cdb383dc5a94db1a0fdb8f8
Evan Gray <evan.gray@rackspace.com>
2014-09-14 11:38:36 -0500

    inserted devise logic for user and app controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::Base
   rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
     render :text => exception, :status => 500
   end
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller? && (resource_name == :user) && action_name == 'new'
+      "devise"
+    else
+      "application"
+    end
+  end
 end
