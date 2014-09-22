d32741741298016168f390b921a607866de148d7
Evan Gray <evan.gray@rackspace.com>
2014-09-21 20:36:12 -0500

    remove old routes and spacing?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  mount Upmin::Engine => '/upmin'
+
+  devise_for :users
+
+  root "reservations#index"
+
   resources :console_servers
 
   resources :mrvs
@@ -8,15 +14,9 @@ Rails.application.routes.draw do
 
   resources :device_console_interfaces
 
-  mount Upmin::Engine => '/upmin'
-
-  root "home#index"
-  devise_for :users
-
-  get 'user_home' => 'user_home#index'
-  get 'user_home/topologies' => 'user_home#topologies'
-
   resources :reservations
+
   resources :topologies
+
   resources :devices
 end
